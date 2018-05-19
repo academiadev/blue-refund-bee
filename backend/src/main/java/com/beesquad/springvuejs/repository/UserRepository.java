7ce1c8c1da1b140aef76276e43df843e0cb606c3
Karll <karllhenning64@gmail.com>
2018-05-19 10:13:52 -0300

    Initial commit

---
+++ backend/src/main/java/com/beesquad/springvuejs/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.beesquad.springvuejs.repository;
+
+//import com.beesquad.springvuejs.model.User;
+
+import java.util.List;
+/*
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	List<User> findByLastName(@Param("lastname") String lastname);
+
+	List<User> findByFirstName(@Param("firstname") String firstname);
+
+}
+*/
---
+++ backend/src/test/java/com/beesquad/springvuejs/SpringVuejsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beesquad.springvuejs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringVuejsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
