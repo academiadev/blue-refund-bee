bf0909bbb519c52a6153c4ec3c752e9ebb1806e2
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2018-06-04 14:37:50 -0300

    Configurado swagger e iniciado o rest com spring

--- api/src/main/java/br/com/academiadev/bluerefund/controller/AdminController.java
+++ api/src/main/java/br/com/academiadev/bluerefund/controller/AdminController.java
@@ -1,19 +1,30 @@
 package br.com.academiadev.bluerefund.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.academiadev.bluerefund.dto.CadastroAdminDTO;
 import br.com.academiadev.bluerefund.exeptions.SenhaInvalidaExeption;
 import br.com.academiadev.bluerefund.service.AdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import br.com.academiadev.bluerefund.exeptions.EmailInvalidoExeption;
 import br.com.academiadev.bluerefund.exeptions.EmailJaCadastradoExeption;
 
+@Api("Admin")
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
 	
 	@Autowired
 	private AdminService adminService;
 
-	public void cadastro(CadastroAdminDTO cadastroAdminDTO) 
+	@ApiOperation(value = "Cadastra um admin e uma empresa")
+	@PostMapping("/cadastro")
+	public void cadastro(@RequestBody CadastroAdminDTO cadastroAdminDTO) 
 			throws SenhaInvalidaExeption, EmailInvalidoExeption, EmailJaCadastradoExeption {
 		adminService.cadastrar(cadastroAdminDTO.getNome(), cadastroAdminDTO.getEmail(), cadastroAdminDTO.getSenha(), cadastroAdminDTO.getNomeEmpresa());
 	}
