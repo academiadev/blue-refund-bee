9ac71fde8ef2bbd2fa0ef3d5958bbfe768414db1
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2018-06-25 16:26:16 -0300

    Revert "Modificado entrada para download de arquivo"
    This reverts commit 8786549b7abf0974f4996ac0c492d0bd41680134.

--- src/main/java/br/com/academiadev/bluerefund/controller/FileUploadController.java
+++ src/main/java/br/com/academiadev/bluerefund/controller/FileUploadController.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -35,7 +35,8 @@ public class FileUploadController {
     
     @GetMapping("/download")
     @ApiOperation(value = "Faz download do arquivo")
-    public ResponseEntity<Resource> download(@RequestBody ArquivoDTO dto) {
+    @ResponseBody
+    public ResponseEntity<Resource> download(ArquivoDTO dto) {
     	Resource file = arquivoService.download(dto);
 		return ResponseEntity.ok()
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
