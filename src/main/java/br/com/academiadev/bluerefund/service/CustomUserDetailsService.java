644d76e875a29fcd4ea021dbd06d4b319fe419ba
Gabriel Mazzoleni Pinto <mpgabriel95@gmail.com>
2018-06-25 16:01:01 -0300

    Deploy

--- src/main/java/br/com/academiadev/bluerefund/service/CustomUserDetailsService.java
+++ src/main/java/br/com/academiadev/bluerefund/service/CustomUserDetailsService.java
@@ -8,9 +8,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.academiadev.bluerefund.config.jwt.ApiPasswordEncoder;
 import br.com.academiadev.bluerefund.model.Usuario;
 import br.com.academiadev.bluerefund.repository.UsuarioRepository;
 
@@ -23,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 	private UsuarioRepository usuarioRepository;
 	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private ApiPasswordEncoder passwordEncoder;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
